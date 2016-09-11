44776eddeeecb37be6b26948e4200612d987fff9
GaryCarneiro <GaryCarneiro@gmail.com>
2016-09-11 23:35:28 +0530

    Adding JS

---
+++ ch1/loan_calc/loancalc.js
@@ -0,0 +1,8 @@
+function calculate() {
+  var amount = document.getElementById("amount");
+  var apr = document.getElementById("apr");
+  var years = document.getElementById("years");
+
+  //console.log(parseInt(amount.value) + parseInt(apr.value));
+
+}
