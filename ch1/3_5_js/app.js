3b3b58c76ab57df17aeba73373324e4ee533cbf0
GaryCarneiro <GaryCarneiro@gmail.com>
2016-09-12 00:19:15 +0530

    Playing with JS Execution context

---
+++ ch1/3_5_js/app.js
@@ -0,0 +1,10 @@
+var a = null; //shows defined
+// var a ; // shows undefined
+console.log(a);
+
+if (a === undefined) {
+    console.log('a is undefined');
+}
+else{
+    console.log('a is defined');
+}
